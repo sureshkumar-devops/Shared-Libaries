f33fa0d4145e816e90574931a182cfc1a1363ae9
Suresh Kumar <87288485+sureshkumar-devops@users.noreply.github.com>
2025-07-06 13:00:08 +0530

    Update and rename updateYamlTag.groovy to updateImageTag.groovy

--- vars/updateImageTag.groovy
+++ vars/updateImageTag.groovy
@@ -1,5 +1,5 @@
 def call(String fileName, String imageName, String dockerTag){
   def sedCommand = " sed -i -e s|${imageName.*}|${imageName}:${dockerTag}|g ${fileName}"
   echo  "Executing: ${sedCommand}"
-  sh sedCommand  
+  //sh sedCommand  
 }
