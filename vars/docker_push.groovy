238c7fd946da3d23f91aff69abb45cbf2c882f91
Suresh Kumar <87288485+sureshkumar-devops@users.noreply.github.com>
2025-07-06 18:52:22 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,9 +1,7 @@
 def call(String ProjectName, String ImageTag, String DockerHubUser)
 {
   //withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) 
-  //withDockerRegistry(credentialsId: 'docker-credentials', toolName: 'DOCKER_HOME')
-  
-  withDockerRegistry(credentialsId: 'wanderlust-token', toolName: 'DOCKER_HOME')
+  withDockerRegistry(credentialsId: 'docker-credentials', toolName: 'DOCKER_HOME')
   {
     //sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
     sh "docker push ${DockerHubUser}/${ProjectName}:${ImageTag}"
