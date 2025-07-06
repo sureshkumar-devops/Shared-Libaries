768ff799a86000425b597cca014065fc64b97542
Suresh Kumar <87288485+sureshkumar-devops@users.noreply.github.com>
2025-07-06 12:11:25 +0530

    Create updateYamlTag.groovy

---
+++ vars/updateYamlTag.groovy
@@ -0,0 +1,5 @@
+def call(String fileName, String imageName, String dockerTag){
+  def sedCommand = " sed -i -e s|${imageName.*}|${imageName}:${dockerTag}|g ${fileName}"
+  echo  "Executing: ${sedCommand}"
+  sh sedCommand  
+}
