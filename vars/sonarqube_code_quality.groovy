71ba49437bb7d51ff2ca4a7bda7e65906640b0b2
Suresh Kumar <87288485+sureshkumar-devops@users.noreply.github.com>
2025-07-07 10:50:11 +0530

    Update sonarqube_code_quality.groovy

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,6 +1,6 @@
 def call(){    
     timeout(time: 5 , unit: "MINUTES"){
-       //waitForQualityGate abortPipeline: true
+        waitForQualityGate abortPipeline: true
         def qg = waitForQualityGate()
         if (qg.status != 'OK'){
            error "Pipeline aborted due to Quality Gate Failure: ${qg.status}"   
